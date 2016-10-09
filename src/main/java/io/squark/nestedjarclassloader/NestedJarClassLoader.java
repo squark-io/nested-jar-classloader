c6323186ff2d65d586aa4600b8187ffc0741c804
Erik H <erikhakan@gmail.com>
2016-10-09 22:40:42 +0200

    updated copyright header

--- src/main/java/io/squark/nestedjarclassloader/NestedJarClassLoader.java
+++ src/main/java/io/squark/nestedjarclassloader/NestedJarClassLoader.java
@@ -1,11 +1,19 @@
-package io.squark.nestedjarclassloader;
-
 /*
-  yggdrasil
-  <p>
-  Created by Erik Håkansson on 2016-04-11.
-  Copyright 2016
+ * Copyright (c) 2016 Erik Håkansson, http://squark.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+package io.squark.nestedjarclassloader;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -19,12 +27,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-/**
- * yggdrasil
- * <p>
- * Created by Erik Håkansson on 2016-04-11.
- * Copyright 2016
- */
 public class NestedJarClassLoader extends ClassLoader {
 
     private static final String DEFAULT_MODULE_NAME = "default";
